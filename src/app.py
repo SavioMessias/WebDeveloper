ec1d22f03ee6f851e4667841a7762daa70b84b67
Manhã <kiqsavio@gmail.com>
2023-10-17 08:07:06 -0300

    Desafio 02

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about_us.html")
+
+@app.route("/contacts")
+def contact():
+    return render_template("contacts.html")
+
+app.run(debug=True)
